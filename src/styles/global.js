c2cb90dd48dadab7abc29087f81d2abd31692794
the-riquelme <riquelmedamiaosilva@gmail.com>
2022-08-04 08:10:56 -0300

    :heavy_plus_sign: build: add and configure editorconfig pattern

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
-export const GlobalStyle = createGlobalStyle `
+export const GlobalStyle = createGlobalStyle`
 	* {
 		margin: 0;
 		padding: 0;
