29df0c3c54ff9b6afa7fab908d9179af77a98e52
Isaac Santiago <eoisaacc@gmail.com>
2022-08-10 00:40:32 -0300

    :sparkles: feat: fetch matches data from API

--- src/pages/Matches/components/Round/index.jsx
+++ src/pages/Matches/components/Round/index.jsx
@@ -17,7 +17,7 @@ import { CaretDown, CaretUp } from 'phosphor-react';
 export const Round = ({ roundNumber, matchesData }) => {
 	const { imagesPath } = useContext(EnviromentContext);
 
-	const [matchesListCollapse, setMatchesListCollapse] = useState(false);
+	const [matchesListCollapse, setMatchesListCollapse] = useState(true);
 
 	const handleMatchesListCollapse = () => {
 		setMatchesListCollapse((prevState) => !prevState);
@@ -40,48 +40,59 @@ export const Round = ({ roundNumber, matchesData }) => {
 				</Button>
 			</RoundHeader>
 			<MatchesList isCollapsed={matchesListCollapse}>
-				{matchesData ? matchesData.map(({ id, stadium, home, away }) => {
+				{matchesData ? matchesData.map(({
+					time_mandante,
+					time_visitante,
+					cartao_vermelho_mandante,
+					cartao_vermelho_visitante,
+					cartao_amarelo_mandante,
+					cartao_amarelo_visitante,
+					gols_mandante,
+					gols_visitante,
+					escudo_mandante,
+					estadio,
+					escudo_visitante }) => {
 					return (
-						<MatchContainer key={id}>
+						<MatchContainer key={`${time_mandante}${estadio}${time_visitante}`}>
 							<CardsWrap>
 								<Card>
 									<img
 										src={`${imagesPath}/yellow-card.svg`}
 										alt="Cartão amarelo"
 									/>
-									<span>{home.yellow_cards}</span>
+									<span>{cartao_amarelo_mandante}</span>
 								</Card>
 								<Card>
 									<img
 										src={`${imagesPath}/red-card.svg`}
 										alt="Cartão vermelho"
 									/>
-									<span>{home.red_cards}</span>
+									<span>{cartao_vermelho_mandante}</span>
 								</Card>
 							</CardsWrap>
 
 							<Team isHome={true}>
-								<span>{home.name}</span>
+								<span>{time_mandante}</span>
 								<img
-									src={`${imagesPath}/datasoccer-logo.svg`}
+									src={escudo_mandante}
 									alt="Cartão amarelo"
 								/>
 							</Team>
 
 							<ScoreBoardWrap>
 								<ScoreBoard>
-									{home.goals}
+									{gols_mandante}
 									<span>x</span>
-									{away.goals}
+									{gols_visitante}
 								</ScoreBoard>
 
-								<span>{stadium}</span>
+								<span>{estadio}</span>
 							</ScoreBoardWrap>
 
 							<Team isHome={false}>
-								<span>{away.name}</span>
+								<span>{time_visitante}</span>
 								<img
-									src={`${imagesPath}/datasoccer-logo.svg`}
+									src={escudo_visitante}
 									alt="Cartão amarelo"
 								/>
 							</Team>
@@ -92,14 +103,14 @@ export const Round = ({ roundNumber, matchesData }) => {
 										src={`${imagesPath}/yellow-card.svg`}
 										alt="Cartão amarelo"
 									/>
-									<span>{away.yellow_cards}</span>
+									<span>{cartao_amarelo_visitante}</span>
 								</Card>
 								<Card>
 									<img
 										src={`${imagesPath}/red-card.svg`}
 										alt="Cartão vermelho"
 									/>
-									<span>{away.red_cards}</span>
+									<span>{cartao_vermelho_visitante}</span>
 								</Card>
 							</CardsWrap>
 						</MatchContainer>
